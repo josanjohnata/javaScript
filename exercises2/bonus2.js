c0b041b528d666cfc5ee7068d791d94a5143b096
josanjohnata <josanjohnata@gmail.com>
2021-07-05 11:22:17 -0300

    Adiciona bonus 3

--- exercises2/bonus2.js
+++ exercises2/bonus2.js
@@ -1,4 +1,4 @@
-let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 1; index < numbers.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
