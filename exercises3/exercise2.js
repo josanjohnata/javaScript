3c432a849a4efba52a31b93b51b3cdc67d3d969a
josanjohnata <josanjohnata@gmail.com>
2021-07-06 09:56:16 -0300

    Adiciona exercicio 2

---
+++ exercises3/exercise2.js
@@ -0,0 +1,8 @@
+let base = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex <= base; lineIndex += 1) {
+  console.log(inputLine);
+  inputLine = inputLine + symbol;
+};
